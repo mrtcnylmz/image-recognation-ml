7dbe82fea024f1d7c07fe481ff803190bba4e251
Mertcan Yılmaz <mert9710@gmail.com>
2022-04-18 18:11:41 +0300

    Initial Commit

---
+++ MachineLearning_IR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning_IR
+//
+//  Created by Mertcan Yılmaz on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
